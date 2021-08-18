7d8bbc9e995830763e34ec99cff68cee7f00feab
sudhaparayil <parayilsudha@gmail.com>
2021-08-18 22:29:48 +0530

    login done

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User; 
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    //
+    function login(Request $req){
+       // return $req->input();
+       $user = User::where(['email'=> $req->email])->first();
+       if(!$user || !Hash::check($req->password,$user->password)){
+           return "username and password is incorrect";
+       }
+       else{
+           $req->session()->put('user',$user);
+           return redirect('/');
+       }
+    }
+}
